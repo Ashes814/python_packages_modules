8488dae77139bca6399029c7fd907920d6c8b209
Owen <1534661514@qq.com>
2022-03-03 09:29:09 +0800

    Initial commit!!!!!

---
+++ other.py
@@ -0,0 +1,7 @@
+import test # 会执行一遍test，在sys.modules创建一个test模块
+print(test.t1) # t1
+print(test.t2) # t2
+
+o1 = 'o1'
+o2 = 'o2'
+
---
+++ test.py
@@ -0,0 +1,37 @@
+# class Student:
+#     def __init__(self, name, student_code, major):
+#         self.name = name
+#         self.student_code = student_code
+#         self.major = major
+#
+#
+# name = '张欧文'
+# student_code = '170111961'
+# major = '''
+#         地理科学（师范），
+#         除了女生很多，
+#         没有任何特色。
+#         '''
+#
+# Owen = Student(name, student_code, major) # 实例化欧欧
+#
+#
+#
+# print('姓名：',Owen.name)
+# print('学号：',Owen.student_code)
+# print('专业简介：',Owen.major)
+
+# print(dir(__builtins__))
+# print(dir(__builtins__))
+# print(dir(__builtins__))
+
+
+# import other # 会执行一遍ohter，在sys.modules创建一个other模块; 第二次导入执行other
+# print(other.o1)
+# print(other.o2)
+# t1 = 't1'
+# t2 = 't2'
+
+import sys
+
+print(sys.path)
