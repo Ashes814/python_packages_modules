94cae6a53ee202351cdc0667decaebdca50bda77
Owen <1534661514@qq.com>
2022-03-05 16:25:44 +0800

    Create package

---
+++ ashes814/setup.py
@@ -0,0 +1,29 @@
+# from distutils.core import setup
+from setuptools import setup
+
+
+def readme_file():
+    with open('README.rst', encoding='utf-8') as rf:
+        return rf.read()
+
+
+setup(name='ashes814', version='1.0.0',
+      description='It is a really niubi package!',
+      packages=['ashes814'], py_modules=['tool'], author='Ashes',
+      author_email='1534661514@qq.com', long_description=readme_file())
+# long_description = '''this is a verbose lib bala,balabalabalabalabala'
+#                    balabalabalabalabalabalabalabalabalabalabala
+#                    balabalabalabalabalabalabalabalabalabalabalabalabala
+#                    balabalabalabalabalabalabalabalabalabalabalabalabala
+#                    Lorem  Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+#                     Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+#                      Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+#                       Lorem Lorem Lorem Lorem Lorem Lorem
+#                        Lorem Lorem Lorem Lorem Lorem Lorem
+#                         Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+#                         QAQ  QAQ QAQ QAQ QAQ QAQ QAQ QAQ QAQ QAQ
+#                          QAQ QAQ QAQ QAQ QAQ QAQ QAQ QAQ
+#                           QAQ QAQ QAQ QAQ
+#                            QAQ QAQ QAQ QAQ QAQ QAQ QAQ QAQ QAQ QAQ
+#                             QAQ QAQ QAQ QAQ QAQ
+#                    ''')
